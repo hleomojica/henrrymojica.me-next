a19aa80d0f5b9c4856f82707be2e8232a6d2b34c
Henrry Mojica <hleomojica@gmail.com>
2023-11-03 12:51:28 +1100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { GeistSans, GeistMono } from 'geist/font';
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           {children}
+          <Analytics />
         </main>  
       </body>
     </html>
