408250556cda9f000bd5588debe20a075219e57f
Henrry Mojica <hleomojica@gmail.com>
2023-11-06 21:12:51 +1100

    fix: links

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import { Suspense } from 'react';
-import vercel from 'public/images/vercel.jpg';
 import linkedin from 'public/images/linkedin.svg';
 import github from 'public/images/github.svg';
 import { StaticImageData } from "next/image"
@@ -32,7 +31,6 @@ function ArrowIcon() {
     </svg>
   );
 }
-
 interface ChannelLinkProps {
   img: StaticImageData;
   name: string;
@@ -40,16 +38,15 @@ interface ChannelLinkProps {
 }
 
 async function ChannelLink({ img, name, link }: ChannelLinkProps) {
-  let subscribers=12;
 
   return (
     <a
       href={link}
       target="_blank"
-      className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4 w-full"
+      className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-2 py-3 w-full"
     >
-      <div className="flex items-center space-x-3">
-        <div className="relative h-16">
+      <div className="flex items-center space-x-4">
+        <div className="relative">
           <Image
             alt={name}
             src={img}
@@ -58,22 +55,12 @@ async function ChannelLink({ img, name, link }: ChannelLinkProps) {
             sizes="20vw"
             className="border border-neutral-200 dark:border-neutral-700 rounded-full h-12 w-12"
           />
-          <div className="border border-neutral-200 dark:border-neutral-700 rounded-full bg-white inline-flex p-1 relative h-6 w-6 items-center -top-5 -right-6">
-            <img
-              alt="YouTube logo"
-              src="/youtube-logo.svg"
-              width="15"
-              height="11"
-            />
-          </div>
+          
         </div>
         <div className="flex flex-col">
           <p className="font-semibold text-neutral-900 dark:text-neutral-100">
             {name}
           </p>
-          {/* <p className="text-neutral-600 dark:text-neutral-400">
-            {subscribers} subscribers
-          </p> */}
         </div>
       </div>
       <div className="text-neutral-700 dark:text-neutral-300">
