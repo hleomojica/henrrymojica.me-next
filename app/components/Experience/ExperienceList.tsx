71796675e4c2158977befeb74d77c96bcefc7b71
Henrry Mojica <hleomojica@gmail.com>
2023-11-07 14:34:12 +1100

    add : experience module

---
+++ app/components/Experience/ExperienceList.tsx
@@ -0,0 +1,9 @@
+import ExperienceItem from './ExperienceItem'
+
+
+export default function ExperienceList ({ experiences }) {
+    return (
+        <div className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4 w-full h-[98px]">
+        </div>
+    )
+}
