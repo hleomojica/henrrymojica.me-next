d90cc8d829333da3cec15ffe641cb648db568fc9
Henrry Mojica <hleomojica@gmail.com>
2023-11-08 12:19:12 +1100

    add lsit types

--- app/components/Experience/List.tsx
+++ app/components/Experience/List.tsx
@@ -1,10 +1,10 @@
 import Item from './Item';
-import { ExperienceItem } from '@/types';
+import { Experience } from '@/types';
 
 export default function ExperienceList({
   experiences,
 }: {
-  experiences: ExperienceItem[];
+  experiences: Experience[];
 }) {
   return (
     <div className='flex h-[98px] w-full items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800'>
