839210cb6855b031ffeb75b9f43ee7fe7378dbaa
Henrry Mojica <hleomojica@gmail.com>
2024-04-04 14:07:15 +1100

    fix: change layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,32 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import { GeistSans, GeistMono } from 'geist/font';
-import './globals.css'
+import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
-import Script from 'next/script'
+import Script from 'next/script';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Henrry Mojica',
   description: 'Software Engineer',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode 
+  children: React.ReactNode;
 }) {
   return (
-    <html 
-      lang="en"
-      className={`${GeistSans.className} h-screen`}>
+    <html lang='en' className={`${GeistSans.className} h-screen`}>
       <head>
         <Script
-          strategy="afterInteractive"
+          strategy='afterInteractive'
           src={'https://www.googletagmanager.com/gtag/js?id=G-B2W5ZG9KKM'}
         />
         <Script
-          id="gtag-init"
-          strategy="afterInteractive"
+          id='gtag-init'
+          strategy='afterInteractive'
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
@@ -39,12 +37,12 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+      <body className='mx-4 mb-40 mt-8 flex max-w-2xl flex-col antialiased md:flex-row lg:mx-auto'>
+        <main className='mt-6 flex min-w-0 flex-auto flex-col px-2 md:px-0'>
           {children}
           <Analytics />
-        </main>  
+        </main>
       </body>
     </html>
-  )
+  );
 }
